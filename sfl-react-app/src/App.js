63d01623d388e2163f956c40e2c0a8b2a1a8ca85
Patrick <pklein111697@gmail.com>
2024-10-27 23:03:17 -0500

    Project Installation
    Needs Node.js installed.
    Run 'npm install' to install dependencies.
    
    Includes the following:
    React (via create-react-app)
    React-Router
    Redux-Toolkit with React Bindings
    TailWindCSS

---
+++ sfl-react-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Survivor Fantasy League</h1>
+    </div>
+  );
+}
+
+export default App;
