601bf8f131a02862db2018e618c03bcc79782844
Patrick <pklein111697@gmail.com>
2024-11-01 19:11:24 -0500

    Cleaning Up
    Cleaning Up

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const authRoutes = require('./auth');  // Separate file for auth routes
-const router = express.Router();
-
-router.use('/auth', authRoutes);  // Mount auth-related routes
-
-module.exports = router;
