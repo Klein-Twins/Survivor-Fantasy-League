2ea02edc39dc2c9960e809a64484b41a4ac70821
Patrick <pklein111697@gmail.com>
2024-11-02 14:08:42 -0500

    Moved a file and also updated errorHandler to remove logging since it interfered with testing.

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -9,7 +9,7 @@ const { STATUS_CODES, RESPONSE_MESSAGES } = require("../routes/ResponseMessageCo
  * @param {Function} next - Express next middleware function.
  */
 const errorHandler = (err, req, res) => {
-  console.error("Error:", err);
+  //console.error("Error:", err);
 
   const statusCode = err.statusCode || RESPONSE_MESSAGES.GENERAL.INTERNAL_SERVER_ERROR.statusCode;
   const message = err.message || RESPONSE_MESSAGES.GENERAL.INTERNAL_SERVER_ERROR.message;
