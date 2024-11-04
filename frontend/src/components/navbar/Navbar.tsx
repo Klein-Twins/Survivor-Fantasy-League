dfa5814e981a4243126362d97e0e6281db00c532
Patrick <pklein111697@gmail.com>
2024-11-03 22:05:56 -0600

    Refactoring and Removing styles from index.css

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { openModal } from "../../store/slices/modalSlice.ts";
-import { RootState } from "../../store/store.ts";
-
-
+import { openModal } from "../../store/slices/modalSlice";
+import { RootState } from "../../store/store";
 
 const Navbar: React.FC = () => {
     const dispatch = useDispatch();
@@ -22,52 +20,65 @@ const Navbar: React.FC = () => {
         dispatch(openModal({ type: 'logout' }));
     }
 
-
     return (
-        <header className="nav-bar">
-            {/* Navbar Flex Container */}
-            <nav className="flex items-end justify-between">
-
-                {/* Links Flex Container - */}
-                <div className="flex space-x-10 items-end justify-between">
-                    <NavLink className="title" to="/">Survivor Fantasy League</NavLink>
-                    <ul className="flex space-x-4 items-center justify-center">
+        <header className="p-4 bg-gray-100 shadow-md">
+            <nav className="flex flex-col md:flex-row items-center justify-between">
+                {/* Title and Links */}
+                <div className="flex flex-col md:flex-row items-center space-x-8">
+                    <NavLink className="title text-lg font-semibold text-blue-700" to="/">Survivor Fantasy League</NavLink>
+                    <ul className="flex space-x-4">
                         <li>
-                            <NavLink to="/" className="link-button">Home</NavLink>
+                            <NavLink 
+                                to="/" 
+                                className={({ isActive }) => 
+                                    isActive ? "link-button text-blue-500 font-semibold" : "link-button"
+                                }
+                            >
+                                Home
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink to="/survivor-cast" className="link-button">Survivor Cast</NavLink>
+                            <NavLink 
+                                to="/survivor-cast" 
+                                className={({ isActive }) => 
+                                    isActive ? "link-button text-blue-500 font-semibold" : "link-button"
+                                }
+                            >
+                                Survivor Cast
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
-                {!isAuthenticated &&
-                    <ul className="flex space-x-4 items-center justify-end">
-                        <li>
+
+                {/* Auth Buttons */}
+                <div className="flex space-x-4 mt-4 md:mt-0">
+                    {!isAuthenticated ? (
+                        <>
                             <button
                                 onClick={handleLogInClick}
-                                className="login-logout-button"
-                            >Log In</button>
-                        </li>
-                        <li>
+                                className="text-blue-700 hover:text-blue-900"
+                            >
+                                Log In
+                            </button>
                             <button
                                 onClick={handleSignUpClick}
                                 className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                            >Sign Up</button>
-                        </li>
-                    </ul>
-                }
-                {isAuthenticated &&
-                    <div className="flex items-end justify-end">
+                            >
+                                Sign Up
+                            </button>
+                        </>
+                    ) : (
                         <button
                             onClick={handleLogOutClick}
-                            className="login-logout-button"
-                        >Log Out</button>
-                    </div>
-                }
-                {/* Auth Buttons Flex container */}
+                            className="text-red-500 hover:text-red-700"
+                        >
+                            Log Out
+                        </button>
+                    )}
+                </div>
             </nav>
         </header>
-    )
+    );
 }
 
 export default Navbar;
