db0a9a700b02b1abe6472f69a41478b5e26c2148
Patrick <pklein111697@gmail.com>
2024-11-05 16:06:37 -0600

    Navbar styling

---
+++ frontend/src/components/navbar/HamburgerButton.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+
+interface HamburgerButtonProps {
+    isOpen: boolean;
+    onClick: () => void;
+}
+
+const HamburgerButton: React.FC<HamburgerButtonProps> = ({isOpen, onClick}) => {
+    return (
+        <button
+            onClick={onClick}
+            className="flex flex-col items-center justify-center w-10 h-10 focus:outline-none"
+        >
+            <span
+                className={`bg-white h-[.125rem] w-full transition-transform duration-300 ease-in-out ${isOpen ? 'transform translate-y-1.5 rotate-45' : ''}`}
+            />
+            <span
+                className={`bg-white h-[.125rem] w-full transition-opacity duration-300 ease-in-out my-1 ${isOpen ? 'opacity-0' : 'opacity-100'}`}
+            />
+            <span
+                className={`bg-white h-[.125rem] w-full transition-transform duration-300 ease-in-out ${isOpen ? 'transform -translate-y-1.5 -rotate-45' : ''}`}
+            />
+        </button>
+    );
+}
+
+export default HamburgerButton;
