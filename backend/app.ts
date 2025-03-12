ddfbc7b310fd5eac0229f1db5b552134218a40db
Patrick <pklein111697@gmail.com>
2025-03-11 23:10:55 -0500

    Cors changes

--- backend/app.ts
+++ backend/app.ts
@@ -9,8 +9,8 @@ import errorHandler from './src/middleware/errorHandlerMiddleware.ts';
 import routes from './src/routes/index.ts';
 
 const corsOptions: cors.CorsOptions = {
-  origin: 'http://localhost:5173', // Replace with frontend URL in production
-  credentials: true
+  origin: ['http://134.199.141.129', 'http://localhost:5173'], // Replace with frontend URL in production
+  credentials: true,
 };
 
 const app = express();
@@ -18,17 +18,20 @@ const app = express();
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 // app.use(requestLogger);
 
 // Load routes
-app.use('/api', routes);  // Mount all routes under '/api'
+app.use('/api', routes); // Mount all routes under '/api'
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get('/swagger.json', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(swaggerSpec); // Send the Swagger JSON specification
 });
-app.use('/images/survivors', express.static(path.join(__dirname, 'src/assets/images/survivors')));
+app.use(
+  '/images/survivors',
+  express.static(path.join(__dirname, 'src/assets/images/survivors'))
+);
 
 app.use(errorHandler);
 
@@ -37,14 +40,13 @@ app.get('/', (req, res) => {
   res.send('Backend is running');
 });
 
-
 const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+    origin: ['http://134.199.141.129', 'http://localhost:5173'],
+    methods: ['GET', 'POST'],
     credentials: true,
-  }
+  },
 });
 
 export const userSockets: Map<string, string> = new Map();
@@ -53,12 +55,12 @@ io.on('connection', (socket) => {
   const profileId = socket.handshake.query.profileId;
   console.log('The following profileId connected to socket: ' + profileId);
 
-  userSockets.set(profileId as string, socket.id)
+  userSockets.set(profileId as string, socket.id);
 
   socket.on('disconnect', () => {
     console.log('The following profileId disconnected to socket: ' + profileId);
-    userSockets.delete(profileId as string)
+    userSockets.delete(profileId as string);
   });
-})
+});
 
 export default server;
