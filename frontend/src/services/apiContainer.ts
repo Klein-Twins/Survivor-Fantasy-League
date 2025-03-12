791350c177e1caed91418ca6ab75f57c9695687f
Patrick <pklein111697@gmail.com>
2025-03-12 01:43:31 -0500

    test

--- frontend/src/services/apiContainer.ts
+++ frontend/src/services/apiContainer.ts
@@ -17,8 +17,6 @@ if (NODE_ENV === 'deployed') {
   basePath = 'http://134.199.141.129:3000';
 }
 
-console.log(NODE_ENV);
-console.log('API base path:', basePath);
 const config = new Configuration({
   basePath,
 });
